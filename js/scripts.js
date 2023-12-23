cd5a5a43afc763f24274d1c2194ab0ebe86b188d
dmitriy <st107687@student.spbu.ru>
2023-12-23 11:54:19 +0300

    Fixes

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,20 @@ scrollButton.addEventListener('click', () => {
     behavior: 'smooth' // Добавим плавности прокрутке
   });
 });
+
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('sendMessageButton')
+
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible m-4" role="alert">' + message
+
+  alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', function () {
+    alert('Спасибо, ваше сообщение отправлено в космос!', 'success')
+  })
+}
+
